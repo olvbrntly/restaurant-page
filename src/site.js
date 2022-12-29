75ac8929568fd7ab29fdfec3c2176f150ebfe455
olive <olivia.brantley9@gmail.com>
2022-12-28 20:08:49 -0800

    making render function/header

--- src/site.js
+++ src/site.js
@@ -1,21 +1,22 @@
 function renderPage() {
   const body = document.querySelector('body');
-
+  const content = document.getElementById('content');
   const btnDiv = document.createElement('div');
   btnDiv.setAttribute('id', 'btnDiv');
+
   const homeBtn = document.createElement('button');
   const menuBtn = document.createElement('button');
   const contactBtn = document.createElement('button');
 
-  homeBtn.textContent = 'home';
-  menuBtn.textContent = 'menu';
-  contactBtn.textContent = 'contact';
+  homeBtn.textContent = 'HOME';
+  menuBtn.textContent = 'MENU';
+  contactBtn.textContent = 'CONTACT';
 
   btnDiv.appendChild(homeBtn);
   btnDiv.appendChild(menuBtn);
   btnDiv.appendChild(contactBtn);
 
-  body.appendChild(btnDiv);
+  body.insertBefore(btnDiv, content);
 }
 
 export default renderPage;
