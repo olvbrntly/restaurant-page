fe0fb970919b5a6d3785c0231d2be2360305902c
olivia <olivia.brantley9@gmail.com>
2023-01-03 11:28:47 -0800

    fixed homepage layout

--- src/homepage.js
+++ src/homepage.js
@@ -5,12 +5,15 @@ function renderHomePage() {
   const content = document.getElementById('content');
   const div = document.createElement('div');
   const title = document.createElement('p');
+  const blurb = document.createElement('p');
 
   div.setAttribute('id', 'hp-div');
   title.setAttribute('id', 'hp-title');
-  title.textContent = 'Welcome to Bestie Bakery';
-
-  content.appendChild(title);
+  blurb.setAttribute('id','blurb');
+  title.textContent = 'Welcome to Sweets Bakery';
+  blurb.textContent = 'We deliver our famous treats all over the country!'
+  div.appendChild(title);
+  div.appendChild(blurb);
   content.appendChild(div);
 }
 
