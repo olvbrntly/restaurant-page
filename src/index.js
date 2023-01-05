6c4e01720f81092892aa76980b99e2f926c43a38
olivia <olivia.brantley9@gmail.com>
2023-01-05 11:57:00 -0800

    eventListeners added, contact still missing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+import renderPage from './site';
 import renderHomePage from './homepage';
+import renderMenuPage from './menu'
+import clearPage from './clear';
 
+renderPage();
 renderHomePage();
+
+const homeBtn = document.getElementById('homeBtn');
+const menuBtn = document.getElementById('menuBtn');
+// const contactBtn = document.getElementById('contactBtn');
+
+homeBtn.addEventListener('click', ()=>{
+    clearPage();
+    renderHomePage();
+    console.log('clicked home');
+})
+
+menuBtn.addEventListener('click', ()=>{
+    clearPage();
+    renderMenuPage();
+    console.log('clicked menu');
+})
 
 // import site.js
 // load homepage -- as default --
