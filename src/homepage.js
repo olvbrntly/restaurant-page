389905add819d33c6bba49da6972b42a1e4eded7
olivia <olivia.brantley9@gmail.com>
2023-01-05 11:54:32 -0800

    completed homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,20 +1,24 @@
-import renderPage from './site';
+
 
 function renderHomePage() {
-  renderPage();
   const content = document.getElementById('content');
+
   const div = document.createElement('div');
   const title = document.createElement('p');
   const blurb = document.createElement('p');
 
+  content.setAttribute('class', 'home-content');
   div.setAttribute('id', 'hp-div');
   title.setAttribute('id', 'hp-title');
   blurb.setAttribute('id','blurb');
+
   title.textContent = 'Welcome to Sweets Bakery';
   blurb.textContent = 'We deliver our famous treats all over the country!'
   div.appendChild(title);
   div.appendChild(blurb);
   content.appendChild(div);
+
+
 }
 
 export default renderHomePage;
