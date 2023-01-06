f1863794b6053c218f6dc3dea16381e5f3c9f540
olivia <olivia.brantley9@gmail.com>
2023-01-05 16:38:03 -0800

    created several menu items

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,12 @@
 
 function createMenuItem(name, cost){
+
   const content = document.getElementById('content');
   const item = document.createElement('div');
+  const itemInfo = document.createElement('div');
+
   item.setAttribute('class', 'menu-item');
+  itemInfo.setAttribute('class', 'item-info');
 
   const image = document.createElement('img');
   image.src = `/dist/images/${name.toLowerCase().replace(/\s/g, '')}.jpg`
@@ -14,8 +18,9 @@ function createMenuItem(name, cost){
   price.textContent = `$${cost}`;
 
   item.appendChild(image);
-  item.appendChild(description);
-  item.appendChild(price);
+  item.appendChild(itemInfo);
+  itemInfo.appendChild(description);
+  itemInfo.appendChild(price);
   
   content.appendChild(item);
 } 
@@ -26,11 +31,17 @@ function renderMenuPage() {
   const content = document.getElementById('content');
   
   const title = document.createElement('h1');
-  title.textContent = " OUR MENU"
+  title.textContent = " THIS WEEK'S MENU"
 
   content.setAttribute('class', 'menu-content');
   content.appendChild(title);
+
   createMenuItem('Raspberry Mint Chocolate Cupcake','4');
+  createMenuItem('Macaroons', '5 / 3 pack');
+  createMenuItem('Croissant', '2.50');
+  createMenuItem('Mini Apple Pies', '6');
+  createMenuItem('Chocolate Glazed Donut', '2');
+  createMenuItem('Salted Caramel Ice Cream', '6 / pint');
 }
 
 export default renderMenuPage;
